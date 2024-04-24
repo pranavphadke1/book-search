1979b8de885f684165f4a5e15bf977dde2faca67
Pranav Phadke <pranavmphadke@gmail.com>
2024-04-24 19:55:37 -0400

    book result type

---
+++ book-search/src/types/bookResult.ts
@@ -0,0 +1,36 @@
+export interface BookResult {
+  author_key: string[];
+  author_name: string[];
+  ebook_access: string;
+  ebook_count_i: number;
+  edition_count: number;
+  edition_key: string[];
+  first_publish_year: number;
+  has_fulltext: boolean;
+  isbn: string[];
+  key: string;
+  language: string[];
+  last_modified_i: number;
+  lcc: string[];
+  number_of_pages_median: number;
+  public_scan_b: boolean;
+  publish_date: string[];
+  publish_year: number[];
+  publisher: string[];
+  seed: string[];
+  title: string;
+  title_suggest: string;
+  title_sort: string;
+  type: string;
+  subject: string[];
+  readinglog_count: number;
+  want_to_read_count: number;
+  currently_reading_count: number;
+  already_read_count: number;
+  publisher_facet: string[];
+  subject_facet: string[];
+  _version_: number;
+  lcc_sort: string;
+  author_facet: string[];
+  subject_key: string[];
+}
